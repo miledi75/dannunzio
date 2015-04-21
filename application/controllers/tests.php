ed65cf4cc6a4a4576b3aa6841aa49a79739db2bf
miledi75 <mileto1975@gmail.com>
2015-04-21 13:48:35 +0200

    setup tests for user model

--- application/controllers/tests.php
+++ application/controllers/tests.php
@@ -13,6 +13,20 @@ class tests extends CI_Controller
 		
 		$test = $this->user_model->getArtists();
 		$this->run($test, 'is_array', 'get_artists');
+		$test = $this->user_model->findCustomer('cle');
+		$this->run($test, 'is_array', 'find_customers');
+		$test = $this->user_model->getUsers(10,0);
+		$this->run($test, 'is_array', 'get_users');
+		$test = $this->user_model->countUsers();
+		$this->run($test, 'is_array', 'count_users');
+		$test = $this->user_model->countCustomers();
+		$this->run($test, 'is_int', 'Count Customers');
+		$test = $this->user_model->getCustomers(10,0);
+		$this->run($test, 'is_array', 'get_customers');
+		$test = $this->user_model->getNewCustomers();
+		$this->run($test, 'is_array', 'get new customers');
+		
+		
 		
 		
 	}
